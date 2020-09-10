58ed88ebecb67d60c8e1b3fcc5d52084e9de7248
brukhanda <bruhanda@gmail.com>
2020-09-10 18:17:26 +0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware' => 'ticket.permission'], function () {
+    Route::get('rates', 'RatesController@index');
+});
 
 
 Route::get('ua_dol','TaskController@index');
